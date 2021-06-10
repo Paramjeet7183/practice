6cec5ec69d52a4d64d00a1ba5283977f6e880e59
Paramjeet singh <Parmajeet7183@gmail.com>
2021-06-11 02:25:17 +0530

    adding responsiveness

--- src/Component/Home/Works.js
+++ src/Component/Home/Works.js
@@ -18,15 +18,66 @@ const WorksContainer = styled.section`
   .WorkItem {
     width: 100%;
     height: 100vh;
+    padding-bottom: 80px;
     position: relative;
-    background-color: green;
-    img {
-      display: inline-block;
-      position: absolute;
-      top: 20px;
-      right: 0;
-      max-width: 60%;
-      max-height: 70%;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    .columns {
+      margin-top: 50px;
+      display: flex;
+      flex-direction: row;
+      justify-content: center;
+      width: 100%;
+      height: 100%;
+      .col_1 {
+        width: 40%;
+        height: 100%;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        span {
+          font-family: exil;
+          font-size: 5vw;
+          color: #252525;
+          height: 50%;
+          width: auto;
+        }
+        #worklink {
+          display: flex;
+          flex-direction: column;
+          justify-content: space-between;
+
+          a {
+            text-decoration: none;
+            font-size: 2rem;
+            color: #252525;
+          }
+          span {
+            font-family: grotesk;
+            font-size: 1rem;
+            color: #252525;
+            writing-mode: vertical-lr;
+          }
+        }
+      }
+      .col_2 {
+        background-color: burlywood;
+        width: 60%;
+        height: 100%;
+        display: flex;
+        flex-direction: column;
+        justify-content: flex-start;
+        img {
+          width: 100%;
+          object-fit: contain;
+        }
+        span {
+          line-height: 95%;
+          font-family: exil;
+          font-size: 3vw;
+        }
+      }
     }
   }
 `;
@@ -36,6 +87,25 @@ function Works() {
     <WorksContainer>
       <div className="WorkItem">
         <Line />
+        <div className="columns">
+          <div className="col_1">
+            <span>01/</span>
+            <span id="worklink">
+              <a href="">
+                <i className="fal fa-external-link-square" />
+              </a>
+              <span>WEBDEVELOPMENT</span>
+            </span>
+          </div>
+          <div className="col_2">
+            <img src={bg} />
+            <span>
+              CORONAVIRUS
+              <br />
+              TRACKER
+            </span>
+          </div>
+        </div>
       </div>
     </WorksContainer>
   );
